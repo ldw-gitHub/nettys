6eda844fedb3880474b8655e21fe9e72d2cd5a0a
ldw-gitHub <wy_ldw@163.com>
2019-12-26 18:51:02 +0800

    'nettyserver'

---
+++ nettyserver/src/main/java/com/framework/test/TestHttpServerHandler.java
@@ -0,0 +1,71 @@
+package com.framework.test;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.net.URI;
+
+public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    /**
+     * 读取服务端流返回
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            HttpRequest httpRequest = (HttpRequest) msg;
+            System.out.println("请求方法名：" + httpRequest.method().name());
+            URI uri = new URI(httpRequest.getUri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                //网站图标，浏览器会默认请求
+                System.out.println("请求favicon.ico");
+                return;
+            }
+
+            ByteBuf content = Unpooled.copiedBuffer("Hello word", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            ctx.writeAndFlush(response);
+        }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered");
+        super.channelUnregistered(ctx);
+    }
+}
--- nettyserver/src/main/java/com/framework/test/TestServer.java
+++ nettyserver/src/main/java/com/framework/test/TestServer.java
@@ -1,15 +1,28 @@
 package com.framework.test;
 
-import com.framework.util.DateUtils;
-
-import java.util.Date;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class TestServer {
 
-    public static void main(String[] args) {
-        System.out.println("1111");
-        Date nowDate = DateUtils.getNowDate();
+        public static void main(String[] args) throws Exception {
+            //事件循环组
+            EventLoopGroup bossGroup = new NioEventLoopGroup(); //获取连接
+            EventLoopGroup workerGroup = new NioEventLoopGroup();//处理连接
+            try {
+                ServerBootstrap serverBootstrap = new ServerBootstrap();
+                serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).
+                        childHandler(new TestServerInitializer());//子处理化拦截器
 
+                ChannelFuture sync = serverBootstrap.bind(8899).sync();
+                sync.channel().closeFuture().sync();
+            } finally {
+                bossGroup.shutdownGracefully();
+                workerGroup.shutdownGracefully();
+            }
 
-    }
+        }
 }
